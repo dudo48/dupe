37cfd9a3b532b9096d36881af9c3f1ee4eb58aae
Fady Emad <fadyemad14705@gmail.com>
2025-01-27 17:19:20 +0200

    improve: rename enum

--- src/size.rs
+++ src/size.rs
@@ -1,17 +1,17 @@
 use std::fmt;
 
 enum Unit {
-    KILOBYTE,
-    MEGABYTE,
-    GIGABYTE,
+    Kilobyte,
+    Megabyte,
+    Gigabyte,
 }
 
 impl fmt::Display for Unit {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let suffix = match self {
-            Unit::KILOBYTE => "Kb",
-            Unit::MEGABYTE => "Mb",
-            Unit::GIGABYTE => "Gb",
+            Unit::Kilobyte => "Kb",
+            Unit::Megabyte => "Mb",
+            Unit::Gigabyte => "Gb",
         };
         return write!(f, "{suffix}");
     }
@@ -25,9 +25,9 @@ pub struct Size {
 impl Size {
     pub fn new(size: u64) -> Self {
         let unit = match size {
-            0..1_000_000 => Unit::KILOBYTE,
-            1_000_000..1_000_000_000 => Unit::MEGABYTE,
-            _ => Unit::GIGABYTE,
+            0..1_000_000 => Unit::Kilobyte,
+            1_000_000..1_000_000_000 => Unit::Megabyte,
+            _ => Unit::Gigabyte,
         };
         return Size { size, unit };
     }
@@ -37,9 +37,9 @@ impl fmt::Display for Size {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let rep_size = (self.size as f32)
             / match self.unit {
-                Unit::KILOBYTE => 1_000.0,
-                Unit::MEGABYTE => 1_000_000.0,
-                Unit::GIGABYTE => 1_000_000_000.0,
+                Unit::Kilobyte => 1_000.0,
+                Unit::Megabyte => 1_000_000.0,
+                Unit::Gigabyte => 1_000_000_000.0,
             };
         return write!(f, "{:.2}{}", rep_size, self.unit);
     }
